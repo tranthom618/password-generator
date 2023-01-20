bc360b2409ba6b8688830d5731e185c4c6a9fbbf
Thomas Tran <tranthom618@gmail.com>
2023-01-19 23:15:46 -0500

    Base code to collect criteria from the user via window prompts. README Template added

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # password-generator
+
+## Description
+
+A password generator is an interesting challenge for a starting developer. 
+
+## Installation
+
+For the purposes of this project, the web application will be hosted on GitHub Pages (https://tranthom618.github.io/password-generator/)
+
+
+## Usage
+
+![Preview of navigation links](assets/images/readme-instructional.png?raw=true "README Example")
+
+Simply follow the instructions on-screen to generate a random password. There is a selection process for your vocabularly of characters that'll be available for use in your password.
+
+## Credits
+
+Starter code provided by University of Toronto - School of Continuing Studies - Coding Bootcamp.
+
+## License
+
+MIT License - See LICENSE file contained within directory.
--- assets/js/script.js
+++ assets/js/script.js
@@ -10,5 +10,29 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  var lengthPW = prompt("Please enter a number for how long your password will be (8 min to 128max):", 8);
+  var lowercase = confirm("Include lowercase letters?\nClick Ok for YES\nClick Cancel for NO");
+  var uppercase = confirm("Include UPPERCASE letters?\nClick Ok for YES\nClick Cancel for NO");
+  var numPW = confirm("Include numbers?\nClick Ok for YES\nClick Cancel for NO");
+  var lowercase = confirm("Include special characters? (!@#$...)\nClick Ok for YES\nClick Cancel for NO");
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+
+/////////////////////////
+
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
